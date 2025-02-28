2d6a9d706ddf1c3b45688f3218329c7b58e31607
DeAsia7 <asiamallory277@gmail.com>
2025-02-27 19:56:02 -0500

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+import {add, subtract, multiply, divide} from './mathUtils.js'; 
+
+
+console.log(add(1, 2)); // 3
+console.log(subtract(3, 2)); // 1
+console.log(multiply(2, 2)); // 4
+console.log(divide(4, 2)); // 2
