e2bfbf2429341358cd8a01bcb7ea134c5bb013c4
DeAsia7 <asiamallory277@gmail.com>
2025-02-27 21:08:44 -0500

    update

--- todolist/main.js
+++ todolist/main.js
@@ -1,14 +1,32 @@
-//cd ..
+import inquirer from 'inquirer';
+import _ from 'lodash';
+import {addTask, markTaskAsDone, deleteTasks, veiwTask} from '.todoList.js';
 
-const inquirer = require('inquirer');
+let task = [];
 
-inquirer.prompt([
+async function mainMenu(){
 
-{type: 'input' , name: username, message: `What is your name?`},
-{ type: 'list', name: 'langauge', message: `What is your favorite programming language?`, 
-    choices: ['JavaScript', 'Python', 'Java', 'C#', 'Ruby'] }
-
-]).then(answers => {
-
-console.log(`hello, ${answers.username}. you love ${answers.language}!`);
+const answer = await inquirer.prompt({
+    type: 'list',
+    name: 'action',
+    message: 'what would you like to do?',
+    choices: ['Add Task', 'Mark Task as Done', 'Delete Task', 'View Task', 'Exit']
 });
+
+if(answer.action === "Add Task"){
+ const task = await inquirer.prompt([
+{type: 'input', name: "task", message: "Enter Task:"}
+ ]);
+ tasks = addTask(tasks, task);
+
+} else if(answer.action === 'Mark Task as Done'){
+const {index} = 
+
+} else if(answer.action === 'Delete Task'){
+
+} else if(answer.action === 'View Task'){
+
+} else {
+
+}
+}
---
+++ todolist/todoList.js
@@ -0,0 +1,16 @@
+export function addTask() {
+
+}
+
+export function markTaskAsDone() {
+
+}
+
+export function deleteTasks() {
+
+}
+
+export function viewTask() {
+
+}
+
