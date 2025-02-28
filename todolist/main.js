737a62f8795ec6a9f3d3d61b7e92c0c191f3b84b
DeAsia7 <asiamallory277@gmail.com>
2025-02-27 22:26:14 -0500

    updste

--- todolist/main.js
+++ todolist/main.js
@@ -3,40 +3,54 @@ import _ from 'lodash';
 import {addTask, markTaskAsDone, deleteTasks, veiwTask} from '.todoList.js';
 
 let task = [];
+ const flag = true;
 
 async function mainMenu(){
+    while(flag == true){
 
 const answer = await inquirer.prompt({
     type: 'list',
-    name: 'action',
-    message: 'what would you like to do?',
-    choices: ['Add Task', 'Mark Task as Done', 'Delete Task', 'View Task', 'Exit']
+    name: "task",
+    message: "what would you like to do?",
+    choices: ["Add Task", "Mark Task as Done", "Delete Task", "View Task", "Exit"]
 });
+    }
 
 if(answer.action === "Add Task"){
- const task = await inquirer.prompt([
-{type: 'input', name: "task", message: "Enter Task:"}
+ const {task} = await inquirer.prompt([
+{type: 'input', 
+    name: "task",
+     message: "Enter Task:"}
  ]);
  tasks = addTask(tasks, task);
 
-} else if(answer.action === 'Mark Task as Done'){
+} else if (answer.action === 'Mark Task as Done'){
 const {index} = await inquirer.prompt([
-    {type: "number" , name: "index", message: "Enter task index to mark as 'done':"}
+    {type: "number",
+     name: "index", 
+     message: "Enter task index to mark as 'done':"}
 ])
 tasks = markTaskAsDone(tasks, index);
-} else if(answer.action === 'Delete Task'){
 
+} else if(answer.action === 'Delete Task'){
     const {index} = await inquirer.prompt([
-        {type: "number" , name: "index", message: "Enter task index to delete:"}
+        {type: "number",
+             name: "index", 
+             message: "Enter task index to delete:"}
     ]);
-} else if(answer.action === 'View Task'){
+
+} else if (answer.action === 'View Task'){
 viewTasks(tasks);
+
 } else {
 console.log('Goodbye!');
+flag == false;
 return;
 }
 }
 
+mainMenu();
 
 // const {sale} = {item: "perfume" , cost: 19.99, sale: true }
 //sale = true
+ 
--- todolist/todoList.js
+++ todolist/todoList.js
@@ -1,16 +1,49 @@
-export function addTask() {
+export function addTask(tacks, task) {
+    const taskObject = {
+        "name": task, //string
+        "done": false //boolean
+    }
+tasks.push(taskObject);
 
+console.log(`new task added: ${taskObject.name}`)
+return tasks;
 }
 
-export function markTaskAsDone() {
-
+export function markTaskAsDone(tasks, index) {
+if(tasks [index]) {
+tasks[index].done = true;
+console.log(`Task marked as done: ${tasks[index].name}!`);
+} else { 
+    console.log("tasks does not exist. please check index value.")
+}
 }
 
-export function deleteTasks() {
-
+export function deleteTasks(tasks, index) {
+if (tasks[index]) {
+  const deletedTasks =  tasks.splice(index, 1); 
+  console.log(`task has been deleted: ${deletedTask[0].name}`)
+} else { 
+    console.log("tasks does not exist. please check index value.")
+}
+return tasks;
 }
 
 export function viewTask() {
+const groupedTasks = _.groupedBy(tasks, "done");
 
+console.log("\n\n\n pending tasks:");
+(groupedTasks[false] || []).forEach((task)), i => 
+console.log(`${i}. ${task.name}`);
+ 
+console.log("\n\n\n Done tasks:");
+(groupedTasks[true] || []).forEach((task), i => 
+console.log(`${i}. ${task.name}`));
 }
 
+
+/*
+define each task
+-action  or whatever the user types
+-the ability to be marked as done
+.splice is taking away how many elements u want. (1) only one will be deleted
+*/
